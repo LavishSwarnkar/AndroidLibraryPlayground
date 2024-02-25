39558407d397a7217b3deb6064ac6561323d36d6
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-02-25 16:56:59 +0530

    import library as a dependency from jitpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    implementation("com.github.LavishSwarnkar.AndroidLibraryPlayground:lib1:1.8")
+    implementation("com.github.LavishSwarnkar.AndroidLibraryPlayground:lib2:1.8")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/lavish/libraryplayground/MainActivity.kt
+++ app/src/main/java/com/lavish/libraryplayground/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lavish.lib1.Adder
 import com.lavish.libraryplayground.ui.theme.LibraryPlaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Adder().add(1, 2)
+
         setContent {
             LibraryPlaygroundTheme {
                 // A surface container using the 'background' color from the theme
