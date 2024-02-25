4ecdc47458236f484026d90e3f714a7107b341cf
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-02-25 09:33:16 +0530

    Initial commit

---
+++ lib1/src/main/java/com/lavish/lib1/Adder.kt
@@ -0,0 +1,7 @@
+package com.lavish.lib1
+
+class Adder {
+
+    fun add(a: Int, b: Int) = a + b
+
+}
---
+++ lib2/src/main/java/com/lavish/lib2/AdderExt.kt
@@ -0,0 +1,5 @@
+package com.lavish.lib2
+
+import com.lavish.lib1.Adder
+
+fun Adder.subtract(a: Int, b: Int) = a - b
