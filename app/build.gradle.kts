968a86222988a010f80a71384b4773666616c7c5
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-03-07 10:25:49 +0530

    test remote dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,11 +53,14 @@ dependencies {
 
     implementation("com.github.LavishSwarnkar.AndroidLibraryPlayground:lib1:1.8")
     implementation("com.github.LavishSwarnkar.AndroidLibraryPlayground:lib2:1.8")
+    implementation("com.github.The-Streamliners.DroidLibs:compose:1.2")
+    implementation("com.github.LavishSwarnkar:JavaLibraryTest:0.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-process:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- app/src/main/java/com/lavish/libraryplayground/MainActivity.kt
+++ app/src/main/java/com/lavish/libraryplayground/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        org.example.LibraryTestClass()
+
         Adder().add(1, 2)
 
         setContent {
